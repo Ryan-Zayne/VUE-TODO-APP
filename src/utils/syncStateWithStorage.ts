a5fc098187c7cc9d35e0a6368b56d56ac7f2f214
Ryan-Zayne <phoenixastro97@gmail.com>
2023-12-31 16:30:23 +0100

    Changed TOmitArray to TPickArray

--- src/utils/syncStateWithStorage.ts
+++ src/utils/syncStateWithStorage.ts
@@ -4,18 +4,18 @@ import { isObject } from './typeof';
 type StorageSyncParams<
 	TKey extends string,
 	TState extends Record<string, unknown>,
-	TOmitArray extends Array<keyof TState>,
+	TPickArray extends Array<keyof TState>,
 > =
 	| [key: TKey, state: string]
 	| [key: TKey, state: TState]
-	| [key: TKey, state: TState, keysToOmit: TOmitArray];
+	| [key: TKey, state: TState, keysToOmit: TPickArray];
 
 const syncStateWithStorage = <
 	TKey extends string,
 	const TState extends Record<string, unknown>,
-	TOmitArray extends Array<keyof TState>,
+	TPickArray extends Array<keyof TState>,
 >(
-	...params: StorageSyncParams<TKey, TState, TOmitArray>
+	...params: StorageSyncParams<TKey, TState, TPickArray>
 ) => {
 	const [storageKey, state, keysToOmit] = params;
 
